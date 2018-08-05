840bde4989e18971ecaae2521d0c29fa6fb1fc79
ddz <dongdezhang01@daojia-inc.com>
2018-08-05 19:01:54 +0800

    add promise comment

--- MyPromise.js
+++ MyPromise.js
@@ -1,10 +1,16 @@
 const PENDING = 0
 const FULFILLED = 1
 const REJECTED = 2
+// 首先定义三种状态，每个promise的状态只允许从pending -> fulfilled 或者 pending -> rejected
 const StaticResolve = function (value) {
+    // 只有当state是Pending时才允许改变
     if (this._state !== PENDING) return
+    // 此时状态从pending -> fulfilled,当前的value 实际上通过 new Promise((resolve, reject) => {resolve(value)}) 这种方式传递过来的
     this._value = value
     this._state = FULFILLED
+    // 这里就相当于调用then(res => console.log(res), rej => console.log(rej))中的 ResolveCallBack 实际是 res => console.log(res) 的一个封装，同理
+    // RejectCallBack 是 rej => console.log(rej) 的一个封装
+    // 这里使用setTimeout 模拟异步，实际上源码中不是这么做的，貌似是用的asap
     setTimeout(() => {
         while (this._deferreds.length > 0) {
             let defer = this._deferreds.shift()
@@ -13,6 +19,7 @@ const StaticResolve = function (value) {
     }, 0)
 
 }
+// 跟 StaticResolve 大同小异
 const StaticReject = function (value) {
     if (this._state !== PENDING) return
     this._value = value
@@ -29,11 +36,16 @@ class MyPromise {
         this._state = PENDING
         this._value = null
         this._deferreds = []
+        // 这里在new 的过程立即调用fun函数，Promise 也是这么做的
         fnc(StaticResolve.bind(this), StaticReject.bind(this))
     }
 
     then(resCallBack, rejCallBack) {
         let _promise = this
+        // then函数执行的时候干了两件事 
+        // 1、将传过来的resCallBack函数和rejCallBack函数封装成一个defer对象，然后push到上一个promise的_deferreds属性中，如果上一个promise状态不为pending，则直接执行
+        // 2、返回一个新的Prosime ---这里理解是一定要记得结合上面的fnc(StaticResolve.bind(this), StaticReject.bind(this))代码
+        // 这些代码都是同步的除了我们setTimeout，也就是说当你在new MyProsime().then()时他们是同步执行的
         return new MyPromise(function (resolve, reject) {
             let defer = {
                 ResolveCallBack(value) {
@@ -80,7 +92,7 @@ class MyPromise {
             }
         })
     }
-
+    // 一下catch，resolve，reject all方法实际上都是在原有已实现代码导航的简单封装
     catch(error) {
         return this.then(() => { }, error)
     }
@@ -115,4 +127,28 @@ class MyPromise {
     }
 }
 
-export default MyPromise
+// export default MyPromise
+// let p1 = new MyPromise((res, rej) => {
+//     setTimeout(() => {
+//         res('delay 5s')
+//     }, 5000)
+// })
+// p1.then(res => {
+//     console.log(res)
+//     return res + ' success'
+// })
+// .then(res => {
+//     console.log(res)
+// })
+// let p2 = new MyPromise((res, rej) => {
+//     setTimeout(() => {
+//         rej('delay 3s')
+//     }, 3000)
+// })
+
+// MyPromise.all([p1, p2])
+// .then(res => {
+//     console.log(res)
+// }, rej => {
+//     console.log(rej)
+// })
