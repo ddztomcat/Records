99c065d871952fbb85fde0bb8b7a65a20dcd1060
unknown <ddztomcat@gmail.com>
2019-07-07 23:05:47 +0800

    :flower:

--- share01/share-virtual-list/src/components/HelloWorld.vue
+++ share01/share-virtual-list/src/components/HelloWorld.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+    <p>sdsdsd
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
